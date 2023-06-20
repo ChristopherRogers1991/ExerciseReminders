3f29be59a03fbe8d184b089fb8adfee98005105f
Chris Rogers <ChristopherRogers1991@gmail.com>
2023-06-20 13:08:24 -0700

    Find and replace gallery -> settings

--- app/src/main/java/nodo/crogers/exercisereminders/ui/settings/SettingsViewModel.java
+++ app/src/main/java/nodo/crogers/exercisereminders/ui/settings/SettingsViewModel.java
@@ -10,7 +10,7 @@ public class SettingsViewModel extends ViewModel {
 
     public SettingsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("This is settings fragment");
     }
 
     public LiveData<String> getText() {
