51815128184f0d377cb7176a88d8226d5b32d8a3
Chris Rogers <ChristopherRogers1991@gmail.com>
2023-06-26 13:40:45 -0700

    Fill out about page

--- app/src/main/java/nodo/crogers/exercisereminders/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/nodo/crogers/exercisereminders/ui/slideshow/SlideshowFragment.java
@@ -1,6 +1,9 @@
 package nodo.crogers.exercisereminders.ui.slideshow;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +13,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import nodo.crogers.exercisereminders.R;
 import nodo.crogers.exercisereminders.databinding.FragmentSlideshowBinding;
 
 public class SlideshowFragment extends Fragment {
@@ -24,8 +28,26 @@ public class SlideshowFragment extends Fragment {
         binding = FragmentSlideshowBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        Spanned sourceCodeLinkHtml = Html.fromHtml(getString(R.string.sourceCodeLink), Html.FROM_HTML_MODE_LEGACY);
+        TextView sourceCodeLink = root.findViewById(R.id.sourceCodeLinkTextView);
+        sourceCodeLink.setText(sourceCodeLinkHtml);
+        sourceCodeLink.setMovementMethod(LinkMovementMethod.getInstance());
+
+        Spanned licenseLinkHtml = Html.fromHtml(getString(R.string.licenseLink), Html.FROM_HTML_MODE_LEGACY);
+        TextView licenceLink = root.findViewById(R.id.licenseLinkTextView);
+        licenceLink.setText(licenseLinkHtml);
+        licenceLink.setMovementMethod(LinkMovementMethod.getInstance());
+
+        Spanned reportAnIssueLinkHtml = Html.fromHtml(getString(R.string.reportAnIssueLink), Html.FROM_HTML_MODE_LEGACY);
+        TextView reportAnIssueLink = root.findViewById(R.id.reportAnIssueLinkTextView);
+        reportAnIssueLink.setText(reportAnIssueLinkHtml);
+        reportAnIssueLink.setMovementMethod(LinkMovementMethod.getInstance());
+
+        Spanned buyMeACoffeeLinkHtml = Html.fromHtml(getString(R.string.buyMeACoffeeLink), Html.FROM_HTML_MODE_LEGACY);
+        TextView buyMeACoffeeLink = root.findViewById(R.id.buyMeACoffeeLinkTextView);
+        buyMeACoffeeLink.setText(buyMeACoffeeLinkHtml);
+        buyMeACoffeeLink.setMovementMethod(LinkMovementMethod.getInstance());
+
         return root;
     }
 
